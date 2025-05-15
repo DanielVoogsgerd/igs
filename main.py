3ee462f41b5b10085b7766b854c67e233300075f
Simon van Noort <50550612+Noorts@users.noreply.github.com>
2025-05-15 21:15:00 +0200

    feat: add base structure (#3)

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ import requests_cache
 import numpy as np
 
 from sources import *
-from utils import grow_extent
 from interface import *
 
 SESSION = requests_cache.CachedSession(
@@ -28,6 +27,7 @@ SESSION = requests_cache.CachedSession(
 
 RESOLUTION = 2**10
 PLATE_CARREE_EPSG = 32662
+MAP_PROJECTION = ccrs.PlateCarree()
 
 
 def main():
@@ -37,59 +37,64 @@ def main():
     lat = np.array([-10.00, -4.75])
     lon = np.array([104.50, 120])
 
-    extent = grow_extent((*lon, *lat), angular_resolution)
-    small_extent = (108, 115, -8, -6)  # for debugging purposes
+    extent = Extent(*lon, *lat).grow_extent(angular_resolution)
     del lon, lat
 
     print("Map extent: ", extent)
 
-    lon_res = int((extent[1] - extent[0]) // angular_resolution + 1)
-    lat_res = int((extent[3] - extent[2]) // angular_resolution + 1)
+    lon_res = int((extent.lon_max - extent.lon_min) // angular_resolution + 1)
+    lat_res = int((extent.lat_max - extent.lat_min) // angular_resolution + 1)
 
     resolution = lat_res, lon_res
     print("Map resolution", resolution)
 
-    map_projection = ccrs.PlateCarree()
+    registry = Registry()
+    registry.register_source(NOAAGfsSource("20250513", "00", 12, "apcpsfc"))
+    registry.register_source(BnpbSource("INDEKS_BAHAYA_BANJIR"))
 
-    precipitation_factory = NOAAGfsSource("20250513", "00", 12, "apcpsfc")
-    rain_data = precipitation_factory.fetch_data(extent, resolution)
+    hazard_index = InAWAREHazardIndex()
 
-    flood_risk_data = rain_data  # Temporary as Bnpb (InaRisk) is down
-    # flood_image_factory = BnpbSource("INDEKS_BAHAYA_BANJIR")
-    # flood_risk_data = flood_image_factory.fetch_data(extent, resolution)
-
-    flood_hazard_index = 0.2 * flood_risk_data * 20 * rain_data * 0.8
-
-    print(type(flood_hazard_index))
-    print(np.max(flood_hazard_index))
-
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1, projection=map_projection)
-    # ax.set_extent((-180, 180, -90, 90))
-    ax.set_extent(extent)
-
-    gdf = gpd.read_file(
-        os.path.join(
-            os.path.dirname(os.path.realpath(__file__)), "cadastre/gadm41_IDN_2.shp"
-        )
-    )
-    gdf = gpd.read_file(
-        os.path.join(
-            os.path.dirname(os.path.realpath(__file__)), "cadastre/gadm41_IDN_2.shp"
-        )
-    )
-
-    bandung = gdf[(gdf["NAME_2"] == "Bandung") & (gdf["TYPE_2"] == "Kabupaten")]
-    bandung.geometry.boundary.plot(ax=ax)
-    bandung = gdf[(gdf["NAME_2"] == "Bandung") & (gdf["TYPE_2"] == "Kabupaten")]
-    bandung.geometry.boundary.plot(ax=ax)
-
-    flood_hazard_index.plot(ax, cmap="Reds")
-    rain_data.plot(ax, cmap="Reds")
-    ax.add_feature(cfeature.COASTLINE)
-    ax.add_feature(cfeature.STATES)
+    # TODO: Link the
 
     plt.show()
+
+
+class InAWAREHazardIndex(HazardIndex):
+    def calculate_index(
+        self, rasters: typing.Dict[SourceIdentifier, IdentifiedRasterizedInformation]
+    ) -> RasterizedInformation:
+        return (
+            rasters["inarisk-flood-risk-index"] * 0.2 * 20
+            + rasters["rain-data-today"] * 0.8
+        )
+
+    @property
+    def required_sources(self) -> typing.List[SourceIdentifier]:
+        return ["rain-data-today", "inarisk-flood-risk-index"]
+
+
+class PlotNotifier(Notifier):
+    def notify(self, notify_raster: RasterizedInformation):
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 1, 1, projection=MAP_PROJECTION)
+        ax.set_extent(notify_raster.extent)
+
+        gdf = gpd.read_file(
+            os.path.join(
+                os.path.dirname(os.path.realpath(__file__)), "cadastre/gadm41_IDN_2.shp"
+            )
+        )
+
+        bandung = gdf[(gdf["NAME_2"] == "Bandung") & (gdf["TYPE_2"] == "Kabupaten")]
+        bandung.geometry.boundary.plot(ax=ax)
+
+        notify_raster.plot(ax, cmap="Reds")
+        ax.add_feature(cfeature.COASTLINE)
+        ax.add_feature(cfeature.STATES)
+
+    @property
+    def responsible_extent(self) -> Extent:
+        return Extent(-180, 180, -90, 90)
 
 
 if __name__ == "__main__":
